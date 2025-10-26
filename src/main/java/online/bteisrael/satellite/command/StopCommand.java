a4ca69e6cd2f788d28afea965bc6aceaf69c2704
NotAMojangDev <git@noreply.namd.dev>
2025-10-26 10:27:43 +0200

    Add Stop command

---
+++ src/main/java/online/bteisrael/satellite/command/StopCommand.java
@@ -0,0 +1,14 @@
+package online.bteisrael.satellite.command;
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.command.builder.Command;
+
+public class StopCommand extends Command {
+    public StopCommand() {
+        super("stop");
+
+        addSyntax((sender, context) -> {
+            MinecraftServer.stopCleanly();
+        });
+    }
+}
