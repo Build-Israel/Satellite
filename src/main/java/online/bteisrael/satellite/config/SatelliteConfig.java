0221a4754d33f276ede8ae93b7a6625d2181f4f8
NotAMojangDev <git@noreply.namd.dev>
2025-10-30 21:04:20 +0200

    Start working on Configuration

--- src/main/java/online/bteisrael/satellite/config/SatelliteConfig.java
+++ src/main/java/online/bteisrael/satellite/config/SatelliteConfig.java
@@ -1,27 +1,22 @@
 package online.bteisrael.satellite.config;
 
 import lombok.Getter;
-import net.kyori.adventure.text.Component;
+import lombok.Setter;
+import online.bteisrael.satellite.SatelliteConstants;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.configurate.objectmapping.ConfigSerializable;
+import org.spongepowered.configurate.objectmapping.meta.Comment;
 
-import java.nio.file.Path;
-
+@ConfigSerializable
 @Getter
+@Setter
 public class SatelliteConfig {
-
-    private final Path configDir;
-    private String secret;
-    private String address;
-    private short port;
-    private Component messageOfTheDay;
-
-    public SatelliteConfig(Path configDir) {
-        this.configDir = configDir;
-        this.reloadConfig();
-    }
-
-    private void reloadConfig() {
-
-    }
-
-
+    @Comment("Please go through this configuration along with " +
+            "this guide: https://go.namd.dev/satellite-cfg ; " +
+            "This config came from a server which was built at: " + SatelliteConstants.BUILD_DATE)
+    private @Nullable String buildTeamName = "BTE";
+    private @Nullable String brandname = "Satellite";
+    private short port = 25565;
+    private String address = "127.0.0.1";
+    private @Nullable String proxySecret = "";
 }
